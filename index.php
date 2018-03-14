c627ccf307317c6f0570a8e52d908abdb4103561
cesardanielhg <cesardanielhg@gmail.com>
2018-03-14 17:38:23 -0600

    Change on decimals php

--- index.php
+++ index.php
@@ -228,13 +228,13 @@
                     $address = $wallet->getAddress();
                     $balance = $wallet->getBalance();
                     
-                    $balance = $balance;
+                    $balance = $balance /1000000000;
 
                     ?>
                         
                     
                     <div class="general_balance">
-                        <h1 class="letter"><?php echo $balance ?></h1>
+                        <h1 class="letter"><?php echo $balance; ?>.00</h1>
                     </div>
 
 
