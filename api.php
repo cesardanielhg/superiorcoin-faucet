e5c9194e2790f649927462b24e4b0970276e57c7
cesardanielhg <cesardanielhg@gmail.com>
2018-03-14 21:31:46 -0600

    Testing api file with address info

--- api.php
+++ api.php
@@ -1,6 +1,43 @@
 <?php
-$option = $_GET['option'];
+$walletAddress = $_GET['walletAddress'];
 
+
+require "../vendor/autoload.php";
+use Superior\Wallet;
+$wallet = new Superior\Wallet();
+
+
+if(isset($_GET['walletAddress']))
+{
+    $userWallet = "Yes";
+} else{
+	$userWallet = "No";
+}
+
+
+
+$address = $wallet->getAddress();
+$balance = $wallet->getBalance();
+
+$getfaucetbal = json_decode($balance);
+$realBalance = number_format($getfaucetbal->{'balance'}/100000000);
+$realBalance = number_format($realBalance, 2, '.', '');
+
+$date = new DateTime();
+$date->format('Y-m-d H:i:s');
+
+
+$response = array();
+$response[0] = array(
+    'userWallet' => $userWallet,
+    'balance'=> $realBalance,
+    'height'=> 'HeightHere',
+    'timeRequest'=>$date
+);
+
+
+
+/*
 if ( $option == 1 ) {
     $data = [ 'a', 'b', 'c' ];
     // will encode to JSON array: ["a","b","c"]
@@ -10,7 +47,8 @@ if ( $option == 1 ) {
     // will encode to JSON object: {"name":"God","age":-1}  
     // accessed as example in JavaScript like: result.name or result['name'] (returns "God")
 }
+*/
 
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($response);
 ?>
